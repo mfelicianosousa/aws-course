de81ff53c4a4fa31125cb0943c868ccdf071729f
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-22 17:25:36 -0400

    Create machine service

---
+++ src/main/java/br/com/mfsdevsystem/awsapi/service/MachineService.java
@@ -0,0 +1,43 @@
+package br.com.mfsdevsystem.awsapi.service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.stereotype.Service;
+
+import br.com.mfsdevsystem.awsapi.entity.DetailedMachine;
+import br.com.mfsdevsystem.awsapi.entity.Machine;
+
+@Service
+public class MachineService {
+
+	// get machine information
+	public Machine getMachineInformation() throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		String ip = address.getHostAddress() ;
+		String name = address.getHostName() ;
+		String os   = System.getProperty("os.name") ;
+		String arch   = System.getProperty("os.arch") ;
+		String osVersion   = System.getProperty("os.version") ;
+		
+		Machine machine = new Machine(ip, name, os, arch, osVersion);
+		
+		return machine;
+	}
+	
+	// get detail machine information
+	public DetailedMachine getDetailedMachineInformation() throws UnknownHostException {
+		InetAddress address = InetAddress.getLocalHost();
+		String ip = address.getHostAddress() ;
+		String name = address.getHostName() ;
+		String os   = System.getProperty("os.name") ;
+		String arch   = System.getProperty("os.arch") ;
+		String osVersion   = System.getProperty("os.version") ;
+		String javaVersion = System.getProperty("java.version");
+		String javaVendor = System.getProperty("java.vendor");
+		String javaVendorUrl = System.getProperty("java.vendor.url");
+		
+		DetailedMachine detailedMachine = new DetailedMachine(ip, name, os, arch, osVersion, javaVersion, javaVendor, javaVendorUrl );
+		return detailedMachine ;
+	}
+}
