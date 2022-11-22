12c80afd7c52a66f0613cff4b36d9accfe0a5b63
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-22 18:30:05 -0400

    Create MachineResource

---
+++ src/main/java/br/com/mfsdevsystem/awsapi/resource/MachineResource.java
@@ -0,0 +1,36 @@
+package br.com.mfsdevsystem.awsapi.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mfsdevsystem.awsapi.entity.DetailedMachine;
+import br.com.mfsdevsystem.awsapi.entity.Machine;
+import br.com.mfsdevsystem.awsapi.service.MachineService;
+
+@RestController
+@RequestMapping(value="machines")
+public class MachineResource {
+	
+	@AutoWired
+	private MachineService machineService;
+	
+	
+	// get basic machine
+	@GetMapping("/basic")
+	public ResponseEntity<Machine> getMachineInformation(){
+		
+		Machine machine = machineService.getMachineInformation();
+		return ResponseEntity.ok(machine) ;
+	}
+	
+	
+	// get detailed machina
+	@GetMapping("/detailed")
+	public ResponseEntity<DetailedMachine> getDetailedMachineInformation(){
+		
+		DetailedMachine detailedMachine = machineService.getDetailedMachineInformation();
+		return ResponseEntity.ok(detailedMachine) ;
+	}
+}
--- src/main/java/br/com/mfsdevsystem/awsapi/service/MachineService.java
+++ src/main/java/br/com/mfsdevsystem/awsapi/service/MachineService.java
@@ -12,32 +12,42 @@ import br.com.mfsdevsystem.awsapi.entity.Machine;
 public class MachineService {
 
 	// get machine information
-	public Machine getMachineInformation() throws UnknownHostException {
-		InetAddress address = InetAddress.getLocalHost();
-		String ip = address.getHostAddress() ;
-		String name = address.getHostName() ;
-		String os   = System.getProperty("os.name") ;
-		String arch   = System.getProperty("os.arch") ;
-		String osVersion   = System.getProperty("os.version") ;
-		
-		Machine machine = new Machine(ip, name, os, arch, osVersion);
-		
-		return machine;
+	public Machine getMachineInformation()  {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			String ip = address.getHostAddress() ;
+			String name = address.getHostName() ;
+			String os   = System.getProperty("os.name") ;
+			String arch   = System.getProperty("os.arch") ;
+			String osVersion   = System.getProperty("os.version") ;
+			
+			Machine machine = new Machine(ip, name, os, arch, osVersion);
+			
+			return machine;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return null ;
+		}
 	}
 	
 	// get detail machine information
-	public DetailedMachine getDetailedMachineInformation() throws UnknownHostException {
-		InetAddress address = InetAddress.getLocalHost();
-		String ip = address.getHostAddress() ;
-		String name = address.getHostName() ;
-		String os   = System.getProperty("os.name") ;
-		String arch   = System.getProperty("os.arch") ;
-		String osVersion   = System.getProperty("os.version") ;
-		String javaVersion = System.getProperty("java.version");
-		String javaVendor = System.getProperty("java.vendor");
-		String javaVendorUrl = System.getProperty("java.vendor.url");
-		
-		DetailedMachine detailedMachine = new DetailedMachine(ip, name, os, arch, osVersion, javaVersion, javaVendor, javaVendorUrl );
-		return detailedMachine ;
+	public DetailedMachine getDetailedMachineInformation() {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			String ip = address.getHostAddress() ;
+			String name = address.getHostName() ;
+			String os   = System.getProperty("os.name") ;
+			String arch   = System.getProperty("os.arch") ;
+			String osVersion   = System.getProperty("os.version") ;
+			String javaVersion = System.getProperty("java.version");
+			String javaVendor = System.getProperty("java.vendor");
+			String javaVendorUrl = System.getProperty("java.vendor.url");
+			
+			DetailedMachine detailedMachine = new DetailedMachine(ip, name, os, arch, osVersion, javaVersion, javaVendor, javaVendorUrl );
+			return detailedMachine ;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return null ;
+		}
 	}
 }
